95d3b67fa9f55e6bc3e8ef0e326a95493ac167e5
Anastasiya <girl-physicist@live.com>
2019-10-21 20:52:38 +0300

    1

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,336 @@ Partial Class Form1
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.ButtonInit = New System.Windows.Forms.Button()
+        Me.ButtonStart = New System.Windows.Forms.Button()
+        Me.ButtonStop = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBoxAlpha0 = New System.Windows.Forms.TextBox()
+        Me.TextBoxV0 = New System.Windows.Forms.TextBox()
+        Me.TextBoxDt = New System.Windows.Forms.TextBox()
+        Me.TextBoxH = New System.Windows.Forms.TextBox()
+        Me.TextBoxXBase = New System.Windows.Forms.TextBox()
+        Me.TextBoxYBase = New System.Windows.Forms.TextBox()
+        Me.Axis1 = New myUCLib.Axis()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBoxXmax = New System.Windows.Forms.TextBox()
+        Me.TextBoxYmax = New System.Windows.Forms.TextBox()
+        Me.TextBoxVmax = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBoxT = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'ButtonInit
+        '
+        Me.ButtonInit.Location = New System.Drawing.Point(705, 13)
+        Me.ButtonInit.Name = "ButtonInit"
+        Me.ButtonInit.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonInit.TabIndex = 0
+        Me.ButtonInit.Text = "Init Axis"
+        Me.ButtonInit.UseVisualStyleBackColor = True
+        '
+        'ButtonStart
+        '
+        Me.ButtonStart.Location = New System.Drawing.Point(705, 43)
+        Me.ButtonStart.Name = "ButtonStart"
+        Me.ButtonStart.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonStart.TabIndex = 1
+        Me.ButtonStart.Text = "Start"
+        Me.ButtonStart.UseVisualStyleBackColor = True
+        '
+        'ButtonStop
+        '
+        Me.ButtonStop.Location = New System.Drawing.Point(705, 72)
+        Me.ButtonStop.Name = "ButtonStop"
+        Me.ButtonStop.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonStop.TabIndex = 1
+        Me.ButtonStop.Text = "Stop"
+        Me.ButtonStop.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'TextBoxAlpha0
+        '
+        Me.TextBoxAlpha0.Location = New System.Drawing.Point(13, 13)
+        Me.TextBoxAlpha0.Name = "TextBoxAlpha0"
+        Me.TextBoxAlpha0.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxAlpha0.TabIndex = 2
+        Me.TextBoxAlpha0.Text = "45"
+        '
+        'TextBoxV0
+        '
+        Me.TextBoxV0.Location = New System.Drawing.Point(13, 39)
+        Me.TextBoxV0.Name = "TextBoxV0"
+        Me.TextBoxV0.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxV0.TabIndex = 2
+        Me.TextBoxV0.Text = "2"
+        '
+        'TextBoxDt
+        '
+        Me.TextBoxDt.Location = New System.Drawing.Point(13, 65)
+        Me.TextBoxDt.Name = "TextBoxDt"
+        Me.TextBoxDt.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxDt.TabIndex = 2
+        Me.TextBoxDt.Text = "0.01"
+        '
+        'TextBoxH
+        '
+        Me.TextBoxH.Location = New System.Drawing.Point(13, 91)
+        Me.TextBoxH.Name = "TextBoxH"
+        Me.TextBoxH.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxH.TabIndex = 2
+        Me.TextBoxH.Text = "0"
+        '
+        'TextBoxXBase
+        '
+        Me.TextBoxXBase.Location = New System.Drawing.Point(12, 117)
+        Me.TextBoxXBase.Name = "TextBoxXBase"
+        Me.TextBoxXBase.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxXBase.TabIndex = 2
+        Me.TextBoxXBase.Text = "8"
+        '
+        'TextBoxYBase
+        '
+        Me.TextBoxYBase.Location = New System.Drawing.Point(12, 143)
+        Me.TextBoxYBase.Name = "TextBoxYBase"
+        Me.TextBoxYBase.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxYBase.TabIndex = 2
+        Me.TextBoxYBase.Text = "8"
+        '
+        'Axis1
+        '
+        Me.Axis1.axis_bkcolor = System.Drawing.Color.White
+        Me.Axis1.axis_color = System.Drawing.Color.Gray
+        Me.Axis1.Axis_Type = CType(1, Byte)
+        Me.Axis1.E_x = 0
+        Me.Axis1.E_y = 0
+        Me.Axis1.Location = New System.Drawing.Point(261, 13)
+        Me.Axis1.Name = "Axis1"
+        Me.Axis1.Pix_color = System.Drawing.Color.Black
+        Me.Axis1.Pix_Size = 2.0!
+        Me.Axis1.Pix_type = CType(1, Byte)
+        Me.Axis1.Size = New System.Drawing.Size(438, 425)
+        Me.Axis1.TabIndex = 3
+        Me.Axis1.x_Base = 1.0!
+        Me.Axis1.x_Name = "X"
+        Me.Axis1.y_Base = 1.0!
+        Me.Axis1.y_Name = "Y"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(120, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Alpha0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(120, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(20, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "V0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(120, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(18, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Dt"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(120, 98)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(15, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "H"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(120, 124)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "XBase"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(120, 150)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(38, 13)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "YBase"
+        '
+        'TextBoxXmax
+        '
+        Me.TextBoxXmax.Location = New System.Drawing.Point(13, 169)
+        Me.TextBoxXmax.Name = "TextBoxXmax"
+        Me.TextBoxXmax.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxXmax.TabIndex = 2
+        '
+        'TextBoxYmax
+        '
+        Me.TextBoxYmax.Location = New System.Drawing.Point(12, 195)
+        Me.TextBoxYmax.Name = "TextBoxYmax"
+        Me.TextBoxYmax.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxYmax.TabIndex = 2
+        '
+        'TextBoxVmax
+        '
+        Me.TextBoxVmax.Location = New System.Drawing.Point(12, 221)
+        Me.TextBoxVmax.Name = "TextBoxVmax"
+        Me.TextBoxVmax.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxVmax.TabIndex = 2
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(120, 176)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(33, 13)
+        Me.Label7.TabIndex = 4
+        Me.Label7.Text = "Xmax"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(120, 202)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(33, 13)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "Ymax"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(120, 228)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(38, 13)
+        Me.Label9.TabIndex = 4
+        Me.Label9.Text = "YBase"
+        '
+        'TextBoxT
+        '
+        Me.TextBoxT.Location = New System.Drawing.Point(12, 247)
+        Me.TextBoxT.Name = "TextBoxT"
+        Me.TextBoxT.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxT.TabIndex = 2
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(120, 254)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(14, 13)
+        Me.Label10.TabIndex = 4
+        Me.Label10.Text = "T"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 302)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(130, 17)
+        Me.RadioButton1.TabIndex = 5
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "considering resistance"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(13, 325)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(110, 17)
+        Me.RadioButton2.TabIndex = 5
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "without resistance"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Axis1)
+        Me.Controls.Add(Me.TextBoxT)
+        Me.Controls.Add(Me.TextBoxVmax)
+        Me.Controls.Add(Me.TextBoxYBase)
+        Me.Controls.Add(Me.TextBoxYmax)
+        Me.Controls.Add(Me.TextBoxXBase)
+        Me.Controls.Add(Me.TextBoxXmax)
+        Me.Controls.Add(Me.TextBoxH)
+        Me.Controls.Add(Me.TextBoxDt)
+        Me.Controls.Add(Me.TextBoxV0)
+        Me.Controls.Add(Me.TextBoxAlpha0)
+        Me.Controls.Add(Me.ButtonStop)
+        Me.Controls.Add(Me.ButtonStart)
+        Me.Controls.Add(Me.ButtonInit)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ButtonInit As Button
+    Friend WithEvents ButtonStart As Button
+    Friend WithEvents ButtonStop As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBoxAlpha0 As TextBox
+    Friend WithEvents TextBoxV0 As TextBox
+    Friend WithEvents TextBoxDt As TextBox
+    Friend WithEvents TextBoxH As TextBox
+    Friend WithEvents TextBoxXBase As TextBox
+    Friend WithEvents TextBoxYBase As TextBox
+    Friend WithEvents Axis1 As myUCLib.Axis
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBoxXmax As TextBox
+    Friend WithEvents TextBoxYmax As TextBox
+    Friend WithEvents TextBoxVmax As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBoxT As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,49 @@
-﻿Public Class Form1
+﻿Option Explicit On
+Imports System.Drawing.Color
+Imports System.Math
+Public Class Form1
+    Dim AxisController As AxisController
+    Public WithEvents _dataController As  DataController
+    Private Sub ButtonInitClick(ByVal sender As System.Object, ByVal e As EventArgs) Handles ButtonInit.Click
+        AxisController = New AxisController(Axis1)
+        AxisController.InitAxis(Val(TextBoxXBase.Text), Val(TextBoxYBase.Text))
+        ButtonStart.Enabled = True
+    End Sub
+    Private Sub ButtonStartClick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonStart.Click
+        _dataController = New DataController(Timer1)
+        _dataController.IsResistanceIncluded = RadioButton1.Checked
+        _dataController.InitData(Val(TextBoxAlpha0.Text),Val(TextBoxV0.Text),Val(TextBoxDt.Text),Val(TextBoxH.Text))
+        Timer1.Enabled = True
+    End Sub
+  
+    Private Sub ButtonStopClick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonStop.Click
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub OnDataCalculated() Handles _dataController.DataCalculated
+        AxisController.DrawTrajectory(_dataController.X, _dataController.Y, GetPixColor())
+        AxisController.DrawCore(_dataController.X, _dataController.Y, GetPixColor())
+    End Sub
+
+    Private sub OnTextBoxDataUpdated() Handles _dataController.TextBoxDataUpdated
+        UpdateTextBoxText(TextBoxXmax, _dataController.Xmax)
+        UpdateTextBoxText(TextBoxYmax, _dataController.Ymax)
+        UpdateTextBoxText(TextBoxVmax, _dataController.Vmax)
+    End sub
+
+    Private sub OnTimeTextBoxDataUpdated() Handles _dataController.TimeTextBoxDataUpdated
+        UpdateTextBoxText(TextBoxT, _dataController.T)
+    End sub
+
+    Private Function GetPixColor () As Color
+        If RadioButton1.Checked Then
+            Return Black
+            Else Return Red
+        End If
+    End Function
+
+    Private Sub UpdateTextBoxText(textBox As TextBox, value As Single)
+        textBox.Text = Str(Round(value, 2))
+    End Sub
 
 End Class
